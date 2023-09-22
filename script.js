392561744d74d35b5da1d291750072fdd3eaff38
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-22 08:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr.sort()
+	let close_sum = Number.MAX_VALUE
+	for(let i = 0; i < arr.length-2; i++){
+		let left = i+1, right = arr.length-1
+		while(left < right){
+			let curr_sum = arr[i] + arr[left] + arr[right]
+			if(Math.abs(target-curr_sum) < Math.abs(target-close_sum)){
+				close_sum = curr_sum
+			}
+
+			if(curr_sum < target){
+				left++
+			}
+			else{
+				right--
+			}
+		}
+	}
+
+	return close_sum
 }
 
 module.exports = threeSum;
